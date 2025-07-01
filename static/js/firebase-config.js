0cabf76f20294f3c2d57715b6567463c9e312f73
vignesh aniruth <vigneshanirvh1@gmail.com>
2025-07-02 00:42:38 +0530

    First Commit

---
+++ static/js/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCoL7AS8Og8uS59cVT_sxYOiY7hHn44wXE",
+  authDomain: "vigneshaniruth-ba64b.firebaseapp.com",
+  projectId: "vigneshaniruth-ba64b",
+  storageBucket: "vigneshaniruth-ba64b.firebasestorage.app",
+  messagingSenderId: "173942599225",
+  appId: "1:173942599225:web:5ad050a8592957bec527e8",
+  measurementId: "G-ZE8DYRXNGF"
+};
+firebase.initializeApp(firebaseConfig);
